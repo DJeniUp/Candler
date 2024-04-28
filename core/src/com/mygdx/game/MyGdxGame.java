96e1bd254898991ec928d50feb287882b359e059
DJeniUp <djen2006i@gmail.com>
2024-04-28 21:13:28 +0200

    Add libgdx

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,11 +1,11 @@
-package candler.game;
+package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Candler extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -19,7 +19,7 @@ public class Candler extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 10, 10);
 		batch.end();
 	}
 	
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,15 +1,15 @@
-package candler.game;
+package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import candler.game.Candler;
+import com.mygdx.game.MyGdxGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Candler Game");
-		new Lwjgl3Application(new Candler(), config);
+		config.setTitle("Candler");
+		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
