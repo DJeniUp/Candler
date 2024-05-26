deaa873edc49dff82da8218c506969b36a21e968
SashaT9 <oleksandr.tymkovych@student.uj.edu.pl>
2024-05-26 17:05:32 +0200

    smth

--- Game/core/src/main/java/Skeleton/MenuArtist.java
+++ Game/core/src/main/java/Skeleton/MenuArtist.java
@@ -2,6 +2,7 @@ package Skeleton;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
@@ -13,7 +14,7 @@ public class MenuArtist implements Artist{
     ImageButton playButton;
     Stage stage;
     Manager manager;
-    MenuArtist(Manager manageR, Stage stageArg){
+    MenuArtist(Manager manageR,Stage stageArg){
         manager=manageR;
         stage = stageArg;
         playButton = new ImageButton(new TextureRegionDrawable(new Texture(Gdx.files.internal("Button.png"))));
---
+++ Game/lwjgl3/src/main/java/games/rednblack/candler/lwjgl3/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package games.rednblack.candler.lwjgl3;
+
+import Skeleton.Manager;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Candler");
+        config.setWindowedMode(800, 600);
+        config.useVsync(true);
+        config.setForegroundFPS(60);
+        new Lwjgl3Application(new Manager(), config);
+    }
+}
