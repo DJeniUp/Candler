63ba31dc1e334ff70e27d13da1258bd80fdf9cf1
z1203243 <66776944+DakPro@users.noreply.github.com>
2024-05-26 19:32:02 +0200

    Revert "Merge branch 'main' of https://github.com/DJeniUp/Candler"
    This reverts commit d4c9ec7caa8ab9345f2eb659d9180e1d92bef343, reversing
    changes made to 201b39bcf2bd86702cdd2909cb97373dcdee89dd.

--- Game/core/src/main/java/games/rednblack/candler/MainMenuScreen.java
+++ Game/core/src/main/java/games/rednblack/candler/MainMenuScreen.java
@@ -68,7 +68,7 @@ public class MainMenuScreen extends ApplicationAdapter{
     public void render() {
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER) && !inGame){
+        if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER)){
             inGame = true;
             game.stage = stage;
             game.create();
